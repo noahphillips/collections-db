f0ae728ce0cd5e13ea105a69f59d520073e27636
Noah Phillips <nphillips@johnmcneilstudio.com>
2017-02-06 17:16:18 -0800

    Adds input template

--- server.js
+++ server.js
@@ -1,14 +1,5 @@
 var http            = require('http')
 var ecstatic        = require('ecstatic')
-// var server  = http.createServer(handler)
- 
-// function handler (req, res) {
-//   router(req, res, function () { static(req, res)})
-// }
-
-// server.listen(11001, function () {
-//   console.log('server listening on http://localhost:11001')
-// })
 
 http.createServer(
   ecstatic({ root: __dirname + '/public' })
